678ce3931683ac780c94a33f35713b5fd59035b4
Saichol-ui <54227217+Saichol-ui@users.noreply.github.com>
2019-08-18 12:24:21 +0700

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-	function getData(){
-		value = document.getElementById('in1').value;
-		showData(value);
-	};
-
-	function showData(msg){
-		console.log(msg);
-		document.getElementById('lb1').innerHtml = msg;
-	};
